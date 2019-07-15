4004cff6d9a0cf556d498dcb16f7ee2131a1d05d
126pikaqiu <1260968291@qq.com>
2019-07-15 16:06:44 +0800

    init

---
+++ src/java/controller/AccountServlet.java
@@ -0,0 +1,8 @@
+package java.controller;
+
+/**
+ * @author: jiaxing liu
+ * @Date: 2019/7/15 16:06
+ */
+public class AccountServlet {
+}
---
+++ src/java/dao/AccountDao.java
@@ -0,0 +1,8 @@
+package java.dao;
+
+/**
+ * @author: jiaxing liu
+ * @Date: 2019/7/15 16:06
+ */
+public class AccountDao {
+}
